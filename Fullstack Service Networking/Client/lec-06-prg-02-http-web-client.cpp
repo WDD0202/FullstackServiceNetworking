abd305b87b937099227f02eade565fa8bedb1142
SeoJY <jyjy0111@khu.ac.kr>
2025-11-18 01:29:12 +0900

    2. GET Handler
    GET 핸들러 구현

--- Fullstack Service Networking/Client/lec-06-prg-02-http-web-client.cpp
+++ Fullstack Service Networking/Client/lec-06-prg-02-http-web-client.cpp
@@ -16,5 +16,19 @@ int main()
 	std::cout << http_request->body << std::endl;
 	std::cout << "## GET response [end]" << std::endl;
 
+	std::cout << "## GET request for http://localhost:8080/?var1=9&var2=9" << std::endl;
+	http_request = cli.Get("/?var1=9&var2=9");
+	std::cout << "## GET response [start]" << std::endl;
+	std::cout << http_request->body << std::endl;
+	std::cout << "## GET response [end]" << std::endl;
+
+	//std::cout << "## GET request for http://localhost:8080/ with var1 is 9 and var2 is 9" << std::endl;
+	//http_request = cli.Get("/");
+	//std::cout << "## GET response [start]" << std::endl;
+	//std::cout << http_request->body << std::endl;
+	//std::cout << "## GET response [end]" << std::endl;
+
+
+
 
 }
--- Fullstack Service Networking/Server/lec-06-prg-http-web-server.cpp
+++ Fullstack Service Networking/Server/lec-06-prg-http-web-server.cpp
@@ -13,9 +13,9 @@ void print_http_request_detail(const Request& req)
 	std::cout << "::Client address : " << req.remote_addr << std::endl;
 	std::cout << "::Client port      : " << req.remote_port << std::endl;
 	std::cout << "::Request command  : " << req.method << std::endl;
-	std::cout << "::Request line     : " << req.method + " " + req.path + " HTTP/" + req.version << std::endl;
-	std::cout << "::Request path     : " << req.path << std::endl;
-	std::cout << "::Request version  : " << "HTTP/" << req.version << std::endl;
+	std::cout << "::Request line     : " << req.method + " " + req.path + req.version << std::endl;
+	std::cout << "::Request path     : " << req.target << std::endl;
+	std::cout << "::Request version  : " << req.version << std::endl;
 }
 
 int simple_calc(int para1 = 0, int para2 = 0)
@@ -23,10 +23,9 @@ int simple_calc(int para1 = 0, int para2 = 0)
 	return para1 * para2;
 }
 
-
-std::vector<int> parameter_retrieval(std::string msg)
+std::string parameter_retrieval(std::string msg)
 {
-	std::vector<int> result;
+	std::string result;
 	std::stringstream ss(msg);
 	std::string pair;
 
@@ -50,9 +49,41 @@ int main()
 
 	Server svr;
 
-	svr.Get("/temp", [](const Request& req, Response& res) {
-		res.set_content("Hello World!", "text/plain");
-		});
+	svr.Get("/.*",[](const Request& req, Response& res) {
+		
+		std::cout << "## do_Get() activated." << std::endl;
+
+		print_http_request_detail(req);
+
+		res.set_header("Content-type", "text/html");
+
+		if (req.path.find('?') != std::string::npos)
+		{
+			std::string target = req.target;
+			size_t pos = target.find('?');
+			std::string routine = (pos != std::string::npos) ? target.substr(pos + 1) : "";
+			std::string parameter = parameter_retrieval(routine);
+			int result = simple_calc(int(parameter[0]), int(parameter[1]));
+
+			std::string html_start = "<html>";
+			std::string get_response = "GET request for calculation => " + std::to_string(parameter[0]) + " x " + std::to_string(parameter[1]) + " = " +  std::to_string(result);
+			std::string html_end = "</html>";
+			std::string body = html_start + get_response + html_end;
+			res.set_content(body, "text/html");
+
+			std::cout << "## GET request for calculation => " << parameter[0] << " x " << parameter[1] << " = " << result << ".\n";
+		}
+		else
+		{
+			std::string html_start = "<html>";
+			std::string get_directory = "<p>HTTP Request GET for Path: " + req.target + "</p>";
+			std::string html_end = "</html>";
+			std::string body = html_start + get_directory + html_end;
+			res.set_content(body, "text/html");
+
+			std::cout << "## GET request for directory =>" << req.target << ".\n";
+		}
+	});
 
 	std::cout << "## HTTP server started at http://" << server_name << server_port << std::endl;
 	svr.listen(server_name, 8080);
