00d827493c0a071e482b13e37bee99e1e8c18e1d
SeoJY <jyjy0111@khu.ac.kr>
2025-12-07 23:12:41 +0900

    #10
    1. crow.h 다시 설치
    2. rest 서버-클라이언트 구현

--- Fullstack Service Networking/Client/lec-06-prg-08-rest-client-v3.cpp
+++ Fullstack Service Networking/Client/lec-06-prg-08-rest-client-v3.cpp
@@ -1,1 +1,61 @@
-#include "crow.h"
+#include "httplib.h"
+#include "json.hpp"
+#include <iostream>
+#include <string>
+
+using namespace httplib;
+using json = nlohmann::json;
+
+
+int main()
+{
+    httplib::Client cli("http://127.0.0.1:5000");
+
+    std::string path_0001 = "/membership_api/0001";
+    std::string path_0002 = "/membership_api/0002";
+
+    httplib::Result r = cli.Get(path_0001.c_str());
+    json reqJson = json::parse(r->body);
+    std::cout << "#1 Code: " << r->status << " >> JSON: " << reqJson << " Json Result: " << reqJson["0001"] << std::endl;
+
+    httplib::Params params1;
+    params1.emplace("0001", "apple");
+    r = cli.Post(path_0001.c_str(), params1);
+    reqJson = json::parse(r->body);
+    std::cout << "#2 Code: " << r->status << " >> JSON: " << reqJson << " Json Result: " << reqJson["0001"] << std::endl;
+
+    r = cli.Get(path_0001.c_str());
+    reqJson = json::parse(r->body);
+    std::cout << "#3 Code: " << r->status << " >> JSON: " << reqJson << " Json Result: " << reqJson["0001"] << std::endl;
+
+    httplib::Params params2;
+    params2.emplace("0001", "xpple");
+    r = cli.Post(path_0001.c_str(), params2);
+    reqJson = json::parse(r->body);
+    std::cout << "#4 Code: " << r->status << " >> JSON: " << reqJson << " Json Result: " << reqJson["0001"] << std::endl;
+
+
+    httplib::Params params3;
+    params3.emplace("0002", "xrange");
+    r = cli.Put(path_0002.c_str(), params3);
+    reqJson = json::parse(r->body);
+    std::cout << "#5 Code: " << r->status << " >> JSON: " << reqJson << " Json Result: " << reqJson["0002"] << std::endl;
+
+    httplib::Params params4;
+    params4.emplace("0002", "orange");
+
+    r = cli.Post(path_0002.c_str(), params3); 
+    r = cli.Put(path_0002.c_str(), params4);  
+    reqJson = json::parse(r->body);   
+    std::cout << "#6 Code: " << r->status << " >> JSON: " << reqJson << " Json Result: " << reqJson["0002"] << std::endl;
+
+    r = cli.Delete(path_0001.c_str());
+    reqJson = json::parse(r->body);
+    std::cout << "#7 Code: " << r->status << " >> JSON: " << reqJson << " Json Result: " << reqJson["0001"] << std::endl;
+
+    r = cli.Delete(path_0001.c_str());
+    reqJson = json::parse(r->body);
+    std::cout << "#8 Code: " << r->status << " >> JSON: " << reqJson << " Json Result: " << reqJson["0001"] << std::endl;
+
+    return 0;
+}
--- Fullstack Service Networking/Server/lec-06-prg-07-rest-server-v3.cpp
+++ Fullstack Service Networking/Server/lec-06-prg-07-rest-server-v3.cpp
@@ -1,1 +1,141 @@
-#include "crow.h"
+#include "crow_all.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+
+std::unordered_map<std::string, std::string> parse_params(const std::string& body)
+{
+    std::unordered_map<std::string, std::string> result;
+    std::string key, value;
+    std::size_t start = 0;
+
+    while (start < body.size()) {
+        std::size_t eq_pos = body.find('=', start);
+        if (eq_pos == std::string::npos) break;
+        key = body.substr(start, eq_pos - start);
+
+        std::size_t amp_pos = body.find('&', eq_pos + 1);
+        if (amp_pos == std::string::npos) {
+            value = body.substr(eq_pos + 1);
+            start = body.size();
+        }
+        else {
+            value = body.substr(eq_pos + 1, amp_pos - (eq_pos + 1));
+            start = amp_pos + 1;
+        }
+        result[key] = value;
+    }
+
+    return result;
+}
+
+
+class MembershipHandler
+{
+private:
+    std::map<std::string, std::string> database;
+
+public:
+    MembershipHandler() = default;
+
+    json create(const std::string& id, const std::string& value)
+    {
+
+        if (database.find(id) != database.end())
+        {
+            return json{ { id, "None" } };
+        }
+        else
+        {
+            database[id] = value;
+            return json{ { id, database[id] } };
+        }
+
+    }
+
+    json read(const std::string& id)
+    {
+
+        if (database.find(id) != database.end())
+        {
+            return json{ { id, database[id] } };
+        }
+        else
+        {
+            return json{ { id, "None" } };
+        }
+
+    }
+
+    json update(const std::string& id, const std::string& value)
+    {
+        json result;
+        if (database.find(id) != database.end())
+        {
+            database[id] = value;
+            return json{ { id, database[id] } };
+        }
+        else
+        {
+            return json{ { id, "None" } };
+        }
+        
+    }
+
+    json Delete(const std::string& id)
+    {
+
+        if (database.find(id) != database.end())
+        {
+            database.erase(id);
+            return json{ { id, "Removed" } };
+        }
+        else
+        {
+            return json{ { id, "None" } };
+        }
+    }
+};
+
+
+int main()
+{
+    crow::SimpleApp app;
+    MembershipHandler myManager;
+
+    CROW_ROUTE(app, "/membership_api/<string>")
+        .methods(crow::HTTPMethod::Get, 
+                 crow::HTTPMethod::Post, 
+                 crow::HTTPMethod::Put, 
+                 crow::HTTPMethod::Delete)
+        ([&](const crow::request& req, std::string member_id) {
+
+        json result;
+
+        if (req.method == crow::HTTPMethod::Post)
+        {
+            std::unordered_map < std::string, std::string> params = parse_params(req.body);
+            std::string value = params.count(member_id) ? params[member_id] : "";
+            result = myManager.create(member_id, value);
+        }
+        else if (req.method == crow::HTTPMethod::Get)
+        {
+            result = myManager.read(member_id);
+        }
+        else if (req.method == crow::HTTPMethod::Put)
+        {
+            std::unordered_map < std::string, std::string> params = parse_params(req.body);
+            std::string value = params.count(member_id) ? params[member_id] : "";
+            result = myManager.update(member_id, value);
+        }
+        else if (req.method == crow::HTTPMethod::Delete)
+        {
+            result = myManager.Delete(member_id);
+        }
+
+        return crow::response(result.dump());
+            });
+
+    app.port(5000).run();
+}
